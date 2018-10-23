3094697c27ef18c5884b324c2a006d6b8c84138a
mazerty <mazerty@gmail.com>
2018-10-23 11:19:02 +0200

    fix #4: added a "parent-containers" section, to use as source instead of official images

--- test/test.py
+++ test/test.py
@@ -10,6 +10,21 @@ def run(command):
     subprocess.Popen(command, shell=True, stdout=sys.stdout, stderr=sys.stderr).wait()
 
 
+# cleans the api output of volatile information before testing
+def clean_container_json(json):
+    json.get("metadata").pop("created_at")
+    json.get("metadata").pop("config")
+    json.get("metadata").pop("last_used_at")
+    json.get("metadata").get("expanded_config").pop("volatile.base_image")
+    json.get("metadata").get("expanded_config").pop("volatile.idmap.next")
+    json.get("metadata").get("expanded_config").pop("volatile.last_state.idmap")
+    json.get("metadata").get("expanded_config").pop("volatile.eth0.hwaddr")
+    json.get("metadata").get("expanded_config").pop("image.description")
+    json.get("metadata").get("expanded_config").pop("image.serial")
+
+    return json
+
+
 # opens a unix socket session to call the lxd api
 session = requests_unixsocket.Session()
 api_url = "http+unix://%2Fvar%2Flib%2Flxd%2Funix.socket"
@@ -28,7 +43,8 @@ assert session.get(api_url + "/1.0/storage-pools/nominal-storage-pool").json() =
                                                                                                 'locations': ['none'],
                                                                                                 'name': 'nominal-storage-pool',
                                                                                                 'status': 'Created',
-                                                                                                'used_by': ['/1.0/containers/dummy-container', '/1.0/profiles/nominal-profile']},
+                                                                                                'used_by': ['/1.0/containers/dummy-container', '/1.0/containers/parent-container',
+                                                                                                            '/1.0/profiles/nominal-profile']},
                                                                                    'operation': '',
                                                                                    'status': 'Success',
                                                                                    'status_code': 200,
@@ -46,7 +62,7 @@ assert session.get(api_url + "/1.0/networks/nominalnetwork0").json() == {'error'
                                                                                       'name': 'nominalnetwork0',
                                                                                       'status': 'Created',
                                                                                       'type': 'bridge',
-                                                                                      'used_by': ['/1.0/containers/dummy-container']},
+                                                                                      'used_by': ['/1.0/containers/dummy-container', '/1.0/containers/parent-container']},
                                                                          'operation': '',
                                                                          'status': 'Success',
                                                                          'status_code': 200,
@@ -62,50 +78,79 @@ assert session.get(api_url + "/1.0/profiles/nominal-profile").json() == {'error'
                                                                                                            'pool': 'nominal-storage-pool',
                                                                                                            'type': 'disk'}},
                                                                                       'name': 'nominal-profile',
-                                                                                      'used_by': ['/1.0/containers/dummy-container']},
+                                                                                      'used_by': ['/1.0/containers/dummy-container', '/1.0/containers/parent-container']},
                                                                          'operation': '',
                                                                          'status': 'Success',
                                                                          'status_code': 200,
                                                                          'type': 'sync'}
-# cleans the api output of volatile information before testing
-container_json = session.get(api_url + "/1.0/containers/dummy-container").json()
-container_json.get("metadata").pop("created_at")
-container_json.get("metadata").pop("config")
-container_json.get("metadata").get("expanded_config").pop("volatile.base_image")
-container_json.get("metadata").get("expanded_config").pop("volatile.idmap.next")
-container_json.get("metadata").get("expanded_config").pop("volatile.last_state.idmap")
-container_json.get("metadata").get("expanded_config").pop("volatile.eth0.hwaddr")
-container_json.get("metadata").get("expanded_config").pop("image.description")
-container_json.get("metadata").get("expanded_config").pop("image.serial")
-assert container_json == {'error': '',
-                          'error_code': 0,
-                          'metadata': {'architecture': 'x86_64',
-                                       'description': '',
-                                       'devices': {},
-                                       'ephemeral': False,
-                                       'expanded_config': {'image.architecture': 'amd64',
-                                                           'image.os': 'Ubuntu',
-                                                           'image.release': 'bionic',
-                                                           'volatile.apply_template': 'create',
-                                                           'volatile.eth0.name': 'eth0',
-                                                           'volatile.idmap.base': '0'},
-                                       'expanded_devices': {'eth0': {'nictype': 'bridged',
-                                                                     'parent': 'nominalnetwork0',
-                                                                     'type': 'nic'},
-                                                            'root': {'path': '/',
-                                                                     'pool': 'nominal-storage-pool',
-                                                                     'type': 'disk'}},
-                                       'last_used_at': '1970-01-01T01:00:00+01:00',
-                                       'location': '',
-                                       'name': 'dummy-container',
-                                       'profiles': ['nominal-profile'],
-                                       'stateful': False,
-                                       'status': 'Stopped',
-                                       'status_code': 102},
-                          'operation': '',
-                          'status': 'Success',
-                          'status_code': 200,
-                          'type': 'sync'}
+assert clean_container_json(session.get(api_url + "/1.0/containers/parent-container").json()) == {'error': '',
+                                                                                                  'error_code': 0,
+                                                                                                  'metadata': {'architecture': 'x86_64',
+                                                                                                               'description': '',
+                                                                                                               'devices': {},
+                                                                                                               'ephemeral': False,
+                                                                                                               'expanded_config': {'image.architecture': 'amd64',
+                                                                                                                                   'image.label': 'daily',
+                                                                                                                                   'image.os': 'ubuntu',
+                                                                                                                                   'image.release': 'bionic',
+                                                                                                                                   'image.version': '18.04',
+                                                                                                                                   'user.user-data': '#cloud-config\n'
+                                                                                                                                                     'runcmd:\n'
+                                                                                                                                                     '  - echo "parent configuration"\n'
+                                                                                                                                                     'power_state:\n'
+                                                                                                                                                     '  mode: poweroff\n',
+                                                                                                                                   'volatile.eth0.name': 'eth0',
+                                                                                                                                   'volatile.idmap.base': '0',
+                                                                                                                                   'volatile.last_state.power': 'STOPPED'},
+                                                                                                               'expanded_devices': {'eth0': {'nictype': 'bridged',
+                                                                                                                                             'parent': 'nominalnetwork0',
+                                                                                                                                             'type': 'nic'},
+                                                                                                                                    'root': {'path': '/',
+                                                                                                                                             'pool': 'nominal-storage-pool',
+                                                                                                                                             'type': 'disk'}},
+                                                                                                               'location': '',
+                                                                                                               'name': 'parent-container',
+                                                                                                               'profiles': ['nominal-profile'],
+                                                                                                               'stateful': False,
+                                                                                                               'status': 'Stopped',
+                                                                                                               'status_code': 102},
+                                                                                                  'operation': '',
+                                                                                                  'status': 'Success',
+                                                                                                  'status_code': 200,
+                                                                                                  'type': 'sync'}
+assert clean_container_json(session.get(api_url + "/1.0/containers/dummy-container").json()) == {'error': '',
+                                                                                                 'error_code': 0,
+                                                                                                 'metadata': {'architecture': 'x86_64',
+                                                                                                              'description': '',
+                                                                                                              'devices': {},
+                                                                                                              'ephemeral': False,
+                                                                                                              'expanded_config': {'image.architecture': 'amd64',
+                                                                                                                                  'image.label': 'daily',
+                                                                                                                                  'image.os': 'ubuntu',
+                                                                                                                                  'image.release': 'bionic',
+                                                                                                                                  'image.version': '18.04',
+                                                                                                                                  'user.user-data': '#cloud-config\n'
+                                                                                                                                                    'runcmd:\n'
+                                                                                                                                                    '  - echo "child configuration"\n',
+                                                                                                                                  'volatile.apply_template': 'copy',
+                                                                                                                                  'volatile.eth0.name': 'eth0',
+                                                                                                                                  'volatile.idmap.base': '0'},
+                                                                                                              'expanded_devices': {'eth0': {'nictype': 'bridged',
+                                                                                                                                            'parent': 'nominalnetwork0',
+                                                                                                                                            'type': 'nic'},
+                                                                                                                                   'root': {'path': '/',
+                                                                                                                                            'pool': 'nominal-storage-pool',
+                                                                                                                                            'type': 'disk'}},
+                                                                                                              'location': '',
+                                                                                                              'name': 'dummy-container',
+                                                                                                              'profiles': ['nominal-profile'],
+                                                                                                              'stateful': False,
+                                                                                                              'status': 'Stopped',
+                                                                                                              'status_code': 102},
+                                                                                                 'operation': '',
+                                                                                                 'status': 'Success',
+                                                                                                 'status_code': 200,
+                                                                                                 'type': 'sync'}
 
 # test starting a stack (twice, for idempotence)
 run("cat nominal.yml | ../src/lxd-compose start")
@@ -121,12 +166,14 @@ assert session.get(api_url + "/1.0/containers/dummy-container").json().get("meta
 assert session.get(api_url + "/1.0/storage-pools/nominal-storage-pool").json().get("error_code") == 0
 assert session.get(api_url + "/1.0/networks/nominalnetwork0").json().get("error_code") == 0
 assert session.get(api_url + "/1.0/profiles/nominal-profile").json().get("error_code") == 0
+assert session.get(api_url + "/1.0/containers/parent-container").json().get("error_code") == 0
 assert session.get(api_url + "/1.0/containers/dummy-container").json().get("error_code") == 0
 run("cat nominal.yml | ../src/lxd-compose delete")
 run("cat nominal.yml | ../src/lxd-compose delete")
 assert session.get(api_url + "/1.0/storage-pools/nominal-storage-pool").json().get("error_code") == 404
 assert session.get(api_url + "/1.0/networks/nominalnetwork0").json().get("error_code") == 404
 assert session.get(api_url + "/1.0/profiles/nominal-profile").json().get("error_code") == 404
+assert session.get(api_url + "/1.0/containers/parent-container").json().get("error_code") == 404
 assert session.get(api_url + "/1.0/containers/dummy-container").json().get("error_code") == 404
 
 # tests if an empty configuration is handled without error, even if it does nothing
