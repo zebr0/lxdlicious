4dfde5a95327125159c9a2c880bef3ad42257b5f
mazerty <mazerty@gmail.com>
2021-01-17 15:41:14 +0100

    big rework, dropping the classes

--- tests/test_old.py
+++ tests/test_old.py
@@ -1,8 +1,11 @@
+import subprocess
+import time
+
 import pytest
-import requests_unixsocket
 import zebr0
 
 import zebr0_lxd
+from zebr0_lxd import Collection
 
 LXD_STACK = """
 ---
@@ -54,19 +57,70 @@ def server():
         yield server
 
 
-@pytest.fixture(scope="module")
-def session():
-    with requests_unixsocket.Session() as session:
-        yield session
+def test_exists():
+    client = zebr0_lxd.Client()
+    assert not client.exists(Collection.STORAGE_POOLS, "nominal-storage-pool")
+    subprocess.Popen("lxc storage create nominal-storage-pool dir", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    assert client.exists(Collection.STORAGE_POOLS, "nominal-storage-pool")
+    subprocess.Popen("lxc storage delete nominal-storage-pool", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
 
 
-def test_ok(server, session):
+def test_create():
+    client = zebr0_lxd.Client()
+    assert not client.exists(Collection.STORAGE_POOLS, "nominal-storage-pool")
+    client.create(Collection.STORAGE_POOLS, {"name": "nominal-storage-pool", "driver": "dir"})
+    assert client.exists(Collection.STORAGE_POOLS, "nominal-storage-pool")
+    subprocess.Popen("lxc storage delete nominal-storage-pool", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+
+
+def test_delete():
+    client = zebr0_lxd.Client()
+    subprocess.Popen("lxc storage create nominal-storage-pool dir", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    assert client.exists(Collection.STORAGE_POOLS, "nominal-storage-pool")
+    client.delete(Collection.STORAGE_POOLS, "nominal-storage-pool")
+    assert not client.exists(Collection.STORAGE_POOLS, "nominal-storage-pool")
+
+
+def test_is_running():
+    client = zebr0_lxd.Client()
+    subprocess.Popen("lxc launch ubuntu:focal test", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    time.sleep(1)
+    assert client.is_running("test")
+    subprocess.Popen("lxc stop test", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    time.sleep(1)
+    assert not client.is_running("test")
+    client.delete(Collection.CONTAINERS, "test")
+
+
+def test_start():
+    client = zebr0_lxd.Client()
+    client.create(Collection.CONTAINERS, {"name": "test", "source": {"type": "image", "mode": "pull", "server": "https://cloud-images.ubuntu.com/daily", "protocol": "simplestreams", "alias": "bionic"}})
+    assert not client.is_running("test")
+    client.start("test")
+    time.sleep(1)
+    assert client.is_running("test")
+    subprocess.Popen("lxc stop test", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    time.sleep(1)
+    assert not client.is_running("test")
+    client.delete(Collection.CONTAINERS, "test")
+
+
+def test_stop():
+    client = zebr0_lxd.Client()
+    subprocess.Popen("lxc launch ubuntu:focal test", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    time.sleep(1)
+    assert client.is_running("test")
+    client.stop("test")
+    assert not client.is_running("test")
+    client.delete(Collection.CONTAINERS, "test")
+
+
+def test_ok(server):
+    client = zebr0_lxd.Client()
     server.data = {
         "lxd-stack": LXD_STACK,
         "lxd-user-data": LXD_USER_DATA
     }
-
-    api_url = "http+unix://%2Fvar%2Fsnap%2Flxd%2Fcommon%2Flxd%2Funix.socket"
 
     # ensures the test resources don't exist before continuing
     zebr0_lxd.main("delete -u http://localhost:8000 -l project stage".split())
@@ -74,55 +128,55 @@ def test_ok(server, session):
     # test creating and running a container (twice, for idempotence)
     zebr0_lxd.main("create -u http://localhost:8000 -l project stage".split())
     zebr0_lxd.main("create -u http://localhost:8000 -l project stage".split())
-    assert session.get(api_url + "/1.0/storage-pools/nominal-storage-pool").json() == {'error': '',
+    assert client.session.get(client.url + "/1.0/storage-pools/nominal-storage-pool").json() == {'error': '',
+                                                                                                 'error_code': 0,
+                                                                                                 'metadata': {'config': {'source': '/var/snap/lxd/common/lxd/storage-pools/nominal-storage-pool'},
+                                                                                                              'description': '',
+                                                                                                              'driver': 'dir',
+                                                                                                              'locations': ['none'],
+                                                                                                              'name': 'nominal-storage-pool',
+                                                                                                              'status': 'Created',
+                                                                                                              'used_by': ['/1.0/instances/dummy-container', '/1.0/profiles/nominal-profile']},
+                                                                                                 'operation': '',
+                                                                                                 'status': 'Success',
+                                                                                                 'status_code': 200,
+                                                                                                 'type': 'sync'}
+    assert client.session.get(client.url + "/1.0/networks/nominalnetwork0").json() == {'error': '',
                                                                                        'error_code': 0,
-                                                                                       'metadata': {'config': {'source': '/var/snap/lxd/common/lxd/storage-pools/nominal-storage-pool'},
+                                                                                       'metadata': {'config': {'dns.domain': 'test',
+                                                                                                               'dns.mode': 'dynamic',
+                                                                                                               'ipv4.address': '10.42.254.1/24',
+                                                                                                               'ipv4.nat': 'true',
+                                                                                                               'ipv6.address': 'none'},
                                                                                                     'description': '',
-                                                                                                    'driver': 'dir',
                                                                                                     'locations': ['none'],
-                                                                                                    'name': 'nominal-storage-pool',
+                                                                                                    'managed': True,
+                                                                                                    'name': 'nominalnetwork0',
                                                                                                     'status': 'Created',
+                                                                                                    'type': 'bridge',
                                                                                                     'used_by': ['/1.0/instances/dummy-container', '/1.0/profiles/nominal-profile']},
                                                                                        'operation': '',
                                                                                        'status': 'Success',
                                                                                        'status_code': 200,
                                                                                        'type': 'sync'}
-    assert session.get(api_url + "/1.0/networks/nominalnetwork0").json() == {'error': '',
-                                                                             'error_code': 0,
-                                                                             'metadata': {'config': {'dns.domain': 'test',
-                                                                                                     'dns.mode': 'dynamic',
-                                                                                                     'ipv4.address': '10.42.254.1/24',
-                                                                                                     'ipv4.nat': 'true',
-                                                                                                     'ipv6.address': 'none'},
-                                                                                          'description': '',
-                                                                                          'locations': ['none'],
-                                                                                          'managed': True,
-                                                                                          'name': 'nominalnetwork0',
-                                                                                          'status': 'Created',
-                                                                                          'type': 'bridge',
-                                                                                          'used_by': ['/1.0/instances/dummy-container', '/1.0/profiles/nominal-profile']},
-                                                                             'operation': '',
-                                                                             'status': 'Success',
-                                                                             'status_code': 200,
-                                                                             'type': 'sync'}
-    assert session.get(api_url + "/1.0/profiles/nominal-profile").json() == {'error': '',
-                                                                             'error_code': 0,
-                                                                             'metadata': {'config': {},
-                                                                                          'description': '',
-                                                                                          'devices': {'eth0': {'nictype': 'bridged',
-                                                                                                               'parent': 'nominalnetwork0',
-                                                                                                               'type': 'nic'},
-                                                                                                      'root': {'path': '/',
-                                                                                                               'pool': 'nominal-storage-pool',
-                                                                                                               'type': 'disk'}},
-                                                                                          'name': 'nominal-profile',
-                                                                                          'used_by': ['/1.0/instances/dummy-container']},
-                                                                             'operation': '',
-                                                                             'status': 'Success',
-                                                                             'status_code': 200,
-                                                                             'type': 'sync'}
+    assert client.session.get(client.url + "/1.0/profiles/nominal-profile").json() == {'error': '',
+                                                                                       'error_code': 0,
+                                                                                       'metadata': {'config': {},
+                                                                                                    'description': '',
+                                                                                                    'devices': {'eth0': {'nictype': 'bridged',
+                                                                                                                         'parent': 'nominalnetwork0',
+                                                                                                                         'type': 'nic'},
+                                                                                                                'root': {'path': '/',
+                                                                                                                         'pool': 'nominal-storage-pool',
+                                                                                                                         'type': 'disk'}},
+                                                                                                    'name': 'nominal-profile',
+                                                                                                    'used_by': ['/1.0/instances/dummy-container']},
+                                                                                       'operation': '',
+                                                                                       'status': 'Success',
+                                                                                       'status_code': 200,
+                                                                                       'type': 'sync'}
     # cleans the api output of "random" information before testing
-    container_json = session.get(api_url + "/1.0/containers/dummy-container").json()
+    container_json = client.session.get(client.url + "/1.0/containers/dummy-container").json()
     container_json.get("metadata").pop("created_at")
     container_json.get("metadata").pop("last_used_at")
     container_json.get("metadata").pop("config")
@@ -168,17 +222,13 @@ def test_ok(server, session):
     # test starting a stack (twice, for idempotence)
     zebr0_lxd.main("start -u http://localhost:8000 -l project stage".split())
     zebr0_lxd.main("start -u http://localhost:8000 -l project stage".split())
-    assert session.get(api_url + "/1.0/containers/dummy-container").json().get("metadata").get("status") == "Running"
+    assert client.session.get(client.url + "/1.0/containers/dummy-container").json().get("metadata").get("status") == "Running"
 
     # test stopping a stack (twice, for idempotence)
     zebr0_lxd.main("stop -u http://localhost:8000 -l project stage".split())
     zebr0_lxd.main("stop -u http://localhost:8000 -l project stage".split())
-    assert session.get(api_url + "/1.0/containers/dummy-container").json().get("metadata").get("status") == "Stopped"
+    assert client.session.get(client.url + "/1.0/containers/dummy-container").json().get("metadata").get("status") == "Stopped"
 
     # test deleting a container (twice, for idempotence)
     zebr0_lxd.main("delete -u http://localhost:8000 -l project stage".split())
     zebr0_lxd.main("delete -u http://localhost:8000 -l project stage".split())
-    assert session.get(api_url + "/1.0/storage-pools/nominal-storage-pool").json().get("error_code") == 404
-    assert session.get(api_url + "/1.0/networks/nominalnetwork0").json().get("error_code") == 404
-    assert session.get(api_url + "/1.0/profiles/nominal-profile").json().get("error_code") == 404
-    assert session.get(api_url + "/1.0/containers/dummy-container").json().get("error_code") == 404
--- zebr0_lxd/__init__.py
+++ zebr0_lxd/__init__.py
@@ -1,182 +1,112 @@
-import logging
+import enum
+from pathlib import Path
 from typing import Optional, List
 
 import requests_unixsocket
 import yaml
 import zebr0
 
-api_url = "http+unix://%2Fvar%2Fsnap%2Flxd%2Fcommon%2Flxd%2Funix.socket"
+KEY_DEFAULT = "lxd-stack"
+URL_DEFAULT = "http+unix://%2Fvar%2Fsnap%2Flxd%2Fcommon%2Flxd%2Funix.socket"
 
 
-# parent (abstract) class for all lxd resources
-class Resource:
-    def __init__(self, session, collection_url, resource_config):
-        self._session = session
-        # relative url for this type of lxd resource (for example: "/1.0/containers")
-        self._collection_url = collection_url
-        # the configuration of this specific resource
-        self._resource_config = resource_config
-        # the logger for this resource
-        self._logger = logging.getLogger("zebr0-lxd." + self.__class__.__name__)
+class Collection(str, enum.Enum):
+    STORAGE_POOLS = "/1.0/storage-pools",
+    NETWORKS = "/1.0/networks",
+    PROFILES = "/1.0/profiles",
+    CONTAINERS = "/1.0/containers"
 
-    # creates this specific resource
-    def create(self):
-        self._log("checking")
-        # only does something if the resource doesn't already exist (idempotence)
-        if not self._exists():
-            self._log("creating")
-            self._session.post(self._get_full_collection_url(), json=self._resource_config)
-            if not self._exists():
-                raise Exception("resource creation failed")
 
-    # deletes this specific resource
-    def delete(self):
-        self._log("checking")
-        # only does something if the resource exists (idempotence)
-        if self._exists():
-            self._log("deleting")
-            self._session.delete(self._get_full_element_url())
-            if self._exists():
-                raise Exception("resource deletion failed")
+class Client:
+    def __init__(self, url: str = URL_DEFAULT):
+        self.url = url
 
-    # returns whether this specific resource exists or not...
-    def _exists(self):
-        # by checking if the resource's name is listed in the response of the collection url
+        # this "hook" will be executed after each request
+        # see http://docs.python-requests.org/en/master/user/advanced/#event-hooks
+        def hook(response, **_):
+            response.raise_for_status()
+
+            # this will wait for lxd asynchronous operations to be finished
+            # see https://github.com/lxc/lxd/blob/master/doc/rest-api.md#background-operation
+            if response.json().get("type") == "async":
+                self.session.get(self.url + response.json().get("operation") + "/wait")
+
+        self.session = requests_unixsocket.Session()
+        self.session.hooks["response"].append(hook)
+
+    def exists(self, collection, resource_name):
         return any(filter(
-            lambda a: a == self._get_element_url(),
-            self._session.get(self._get_full_collection_url()).json().get("metadata")
+            lambda a: a == collection + "/" + resource_name,
+            self.session.get(self.url + collection).json().get("metadata")
         ))
 
-    # returns the full url for this type of lxd resource
-    def _get_full_collection_url(self):
-        return api_url + self._collection_url
+    def create(self, collection, resource):
+        if not self.exists(collection, resource.get("name")):
+            self.session.post(self.url + collection, json=resource)
 
-    # returns the resource's name
-    def _get_name(self):
-        return self._resource_config.get("name")
+    def delete(self, collection, resource_name):
+        if self.exists(collection, resource_name):
+            self.session.delete(self.url + collection + "/" + resource_name)
 
-    # returns the resource's relative url
-    def _get_element_url(self):
-        return self._collection_url + "/" + self._get_name()
+    def is_running(self, container_name):
+        return self.session.get(self.url + Collection.CONTAINERS + "/" + container_name).json().get("metadata").get("status") == "Running"
 
-    # returns the resource's full url
-    def _get_full_element_url(self):
-        return self._get_full_collection_url() + "/" + self._get_name()
+    def start(self, container_name):
+        if not self.is_running(container_name):
+            self.session.put(self.url + Collection.CONTAINERS + "/" + container_name + "/state", json={"action": "start"})
 
-    # default logging function
-    def _log(self, action):
-        self._logger.info("%s %s", action, self._get_element_url())
-
-
-class StoragePool(Resource):
-    def __init__(self, session, resource_config):
-        super().__init__(session, "/1.0/storage-pools", resource_config)
+    def stop(self, container_name):
+        if self.is_running(container_name):
+            self.session.put(self.url + Collection.CONTAINERS + "/" + container_name + "/state", json={"action": "stop"})
 
 
-class Network(Resource):
-    def __init__(self, session, resource_config):
-        super().__init__(session, "/1.0/networks", resource_config)
+def create(url: str, levels: Optional[List[str]], cache: int, configuration_file: Path, key: str = KEY_DEFAULT, lxd_url: str = URL_DEFAULT):
+    stack = yaml.load(zebr0.Client(url, levels, cache, configuration_file).get(key), Loader=yaml.BaseLoader)
+    client = Client(lxd_url)
+
+    for resource in stack.get("storage_pools") or []:
+        client.create(Collection.STORAGE_POOLS, resource)
+    for resource in stack.get("networks") or []:
+        client.create(Collection.NETWORKS, resource)
+    for resource in stack.get("profiles") or []:
+        client.create(Collection.PROFILES, resource)
+    for resource in stack.get("containers") or []:
+        client.create(Collection.CONTAINERS, resource)
 
 
-class Profile(Resource):
-    def __init__(self, session, resource_config):
-        super().__init__(session, "/1.0/profiles", resource_config)
+def start(url: str, levels: Optional[List[str]], cache: int, configuration_file: Path, key: str = KEY_DEFAULT, lxd_url: str = URL_DEFAULT):
+    stack = yaml.load(zebr0.Client(url, levels, cache, configuration_file).get(key), Loader=yaml.BaseLoader)
+    client = Client(lxd_url)
+
+    for resource in stack.get("containers") or []:
+        client.start(resource.get("name"))
 
 
-class Container(Resource):
-    def __init__(self, session, resource_config):
-        super().__init__(session, "/1.0/containers", resource_config)
+def stop(url: str, levels: Optional[List[str]], cache: int, configuration_file: Path, key: str = KEY_DEFAULT, lxd_url: str = URL_DEFAULT):
+    stack = yaml.load(zebr0.Client(url, levels, cache, configuration_file).get(key), Loader=yaml.BaseLoader)
+    client = Client(lxd_url)
 
-    # starts the container
-    def start(self):
-        # only does something if the resource isn't already running (idempotence)
-        if not self._is_running():
-            self._log("starting")
-            self._session.put(self._get_full_element_url() + "/state", json={"action": "start"})
-            if not self._is_running():
-                raise Exception("container starting failed")
-
-    # stops the container
-    def stop(self):
-        # only does something if the resource is running (idempotence)
-        if self._is_running():
-            self._log("stopping")
-            self._session.put(self._get_full_element_url() + "/state", json={"action": "stop"})
-            if self._is_running():
-                raise Exception("container stopping failed")
-
-    # returns whether the container is running or not...
-    def _is_running(self):
-        # by checking its status in the metadata
-        return self._exists() and self._session.get(self._get_full_element_url()).json().get("metadata").get("status") == "Running"
+    for resource in stack.get("containers") or []:
+        client.stop(resource.get("name"))
 
 
-def create(session, config):
-    for item in config.get("storage_pools") or []:
-        StoragePool(session, item).create()
-    for item in config.get("networks") or []:
-        Network(session, item).create()
-    for item in config.get("profiles") or []:
-        Profile(session, item).create()
-    for item in config.get("containers") or []:
-        Container(session, item).create()
+def delete(url: str, levels: Optional[List[str]], cache: int, configuration_file: Path, key: str = KEY_DEFAULT, lxd_url: str = URL_DEFAULT):
+    stack = yaml.load(zebr0.Client(url, levels, cache, configuration_file).get(key), Loader=yaml.BaseLoader)
+    client = Client(lxd_url)
 
-
-def start(session, config):
-    for item in config.get("containers") or []:
-        Container(session, item).start()
-
-
-def stop(session, config):
-    for item in config.get("containers") or []:
-        Container(session, item).stop()
-
-
-def delete(session, config):
-    for item in config.get("containers") or []:
-        Container(session, item).delete()
-    for item in config.get("profiles") or []:
-        Profile(session, item).delete()
-    for item in config.get("networks") or []:
-        Network(session, item).delete()
-    for item in config.get("storage_pools") or []:
-        StoragePool(session, item).delete()
+    for resource in stack.get("containers") or []:
+        client.delete(Collection.CONTAINERS, resource.get("name"))
+    for resource in stack.get("profiles") or []:
+        client.delete(Collection.PROFILES, resource.get("name"))
+    for resource in stack.get("networks") or []:
+        client.delete(Collection.NETWORKS, resource.get("name"))
+    for resource in stack.get("storage_pools") or []:
+        client.delete(Collection.STORAGE_POOLS, resource.get("name"))
 
 
 def main(args: Optional[List[str]] = None) -> None:
-    # this "hook" will be executed after each request to allow some generic treatment of the response
-    # see http://docs.python-requests.org/en/master/user/advanced/#event-hooks
-    def hook(response, **_):
-        json = response.json()
-        session_logger.debug(json)
-
-        _type = json.get("type")
-
-        if _type == "error":
-            raise Exception(json)
-
-        # this will wait for lxd asynchronous operations to be finished
-        # see https://github.com/lxc/lxd/blob/master/doc/rest-api.md#background-operation
-        if _type == "async":
-            wait_json = session.get(api_url + json.get("operation") + "/wait").json()
-            if wait_json.get("metadata").get("status_code") != 200:
-                raise Exception(wait_json)
-
-    # opens a unix socket session and adds the hook defined above
-    session = requests_unixsocket.Session()
-    session.hooks["response"].append(hook)
-    session_logger = logging.getLogger("zebr0-lxd.Session")
-
     argparser = zebr0.build_argument_parser(description="zebr0 client to deploy an application to a local LXD environment")
     argparser.add_argument("command", choices=["create", "start", "stop", "delete"])
     args = argparser.parse_args(args)
 
-    zebr0_service = zebr0.Client(args.url, args.levels, args.cache, args.configuration_file)
-
-    # loads the configuration from zebr0 (uses the yaml baseloader to preserve all strings)
-    config = yaml.load(zebr0_service.get("lxd-stack"), Loader=yaml.BaseLoader)
-
-    # calls the method given as parameter
-    globals()[args.command](session, config)
-
-    session.close()
+    globals()[args.command](args.url, args.levels, args.cache, args.configuration_file)
