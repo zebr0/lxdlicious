a2b043747a20f78019556c8e7c5a3b7a2ea088c6
mazerty <mazerty@gmail.com>
2021-01-31 10:18:09 +0100

    comments and docstrings

--- zebr0_lxd/__init__.py
+++ zebr0_lxd/__init__.py
@@ -11,27 +11,58 @@ URL_DEFAULT = "http+unix://%2Fvar%2Fsnap%2Flxd%2Fcommon%2Flxd%2Funix.socket"
 
 
 class Resource(str, enum.Enum):
+    """
+    Enumerates the various LXD resource types managed by the library.
+    """
+
     STORAGE_POOLS = "storage-pools",
     NETWORKS = "networks",
     PROFILES = "profiles",
     INSTANCES = "instances"
 
-    def path(self):
+    def path(self) -> str:
+        """
+        :return: the corresponding path relative to the LXD API base URL
+        """
         return "/1.0/" + self
 
 
 class Client:
+    """
+    A simple wrapper around the LXD REST API to manage resources either directly or via "stacks".
+
+    This Client connects to the LXD API through the Unix socket (for now).
+    Apart from how asynchronous operations are handled, it's mainly a convenient, idempotent passthrough.
+    Therefore, the official documentation is where you'll find all the configuration details you'll need to create LXD resources:
+
+    * storage-pools: https://linuxcontainers.org/lxd/docs/master/rest-api#10storage-pools and https://linuxcontainers.org/lxd/docs/master/storage
+    * networks: https://linuxcontainers.org/lxd/docs/master/rest-api#10networks and https://linuxcontainers.org/lxd/docs/master/networks
+    * profiles: https://linuxcontainers.org/lxd/docs/master/rest-api#10profiles and https://linuxcontainers.org/lxd/docs/master/profiles
+    * instances: https://linuxcontainers.org/lxd/docs/master/rest-api#10instances and https://linuxcontainers.org/lxd/docs/master/instances
+
+    A "stack" is very a convenient way to manage a group of resources linked together.
+    Heavily inspired by the LXD "preseed" format (see https://linuxcontainers.org/lxd/docs/master/preseed), the structure is almost identical, except:
+
+    * "storage_pools" has been renamed "storage-pools" to match the API
+    * the root "config" element is ignored (use a real preseed file if you want to configure LXD that way)
+    * instances are managed through a new root element, "instances"
+
+    A typical stack example can be found in tests/test_cli.py.
+    Check the various functions to see what you can do with stacks and resources.
+
+    :param url: URL of the LXD API (scheme is "http+unix", socket path is percent-encoded into the host field), defaults to "http+unix://%2Fvar%2Fsnap%2Flxd%2Fcommon%2Flxd%2Funix.socket"
+    """
+
     def __init__(self, url: str = URL_DEFAULT):
         self.url = url
 
-        # this "hook" will be executed after each request
-        # see http://docs.python-requests.org/en/master/user/advanced/#event-hooks
+        # this "hook" will be executed after each request (see http://docs.python-requests.org/en/master/user/advanced/#event-hooks)
         def hook(response, **_):
             if not response.ok:
                 raise Exception(response.text)
 
-            # this will wait for lxd asynchronous operations to be finished
-            # see https://github.com/lxc/lxd/blob/master/doc/rest-api.md#background-operation
+            # some lxd operations are asynchronous (see https://linuxcontainers.org/lxd/docs/master/rest-api#async-operations)
+            # this can be problematic so we have to wait for them to finish before continuing (see https://linuxcontainers.org/lxd/docs/master/rest-api#10operationsuuidwait)
             if response.json().get("type") == "async":
                 self.session.get(self.url + response.json().get("operation") + "/wait")
 
@@ -39,51 +70,114 @@ class Client:
         self.session.hooks["response"].append(hook)
 
     def exists(self, resource: Resource, name: str) -> bool:
+        """
+        :param resource: the resource's type
+        :param name: the resource's name
+        :return: whether the resource exists or not
+        """
+
         print(f"checking {resource}/{name}")
         return any(filter(
             lambda a: a == resource.path() + "/" + name,
-            self.session.get(self.url + resource.path()).json().get("metadata")
+            self.session.get(self.url + resource.path()).json().get("metadata")  # returns a list of existing resources
         ))
 
     def create(self, resource: Resource, config: dict) -> None:
+        """
+        Creates a resource if it doesn't exist (based on its name).
+        The required configuration depends on the resource's type (see zebr0_lxd.Client).
+
+        :param resource: the resource's type
+        :param config: the resource's desired configuration
+        """
+
         if not self.exists(resource, config.get("name")):
             print(f"creating {resource}/{json.dumps(config)}")
             self.session.post(self.url + resource.path(), json=config)
 
     def delete(self, resource: Resource, name: str) -> None:
+        """
+        Deletes a resource if it exists (based on its name).
+
+        :param resource: the resource's type
+        :param name: the resource's name
+        """
+
         if self.exists(resource, name):
             print(f"deleting {resource}/{name}")
             self.session.delete(self.url + resource.path() + "/" + name)
 
     def is_running(self, name: str) -> bool:
+        """
+        :param name: the instance's name
+        :return: whether the instance is running or not
+        """
+
         print(f"checking {Resource.INSTANCES}/{name}")
         return self.session.get(self.url + Resource.INSTANCES.path() + "/" + name).json().get("metadata").get("status") == "Running"
 
     def start(self, name: str) -> None:
+        """
+        Starts an instance if it's not running (based on its name).
+
+        :param name: the instance's name
+        """
+
         if not self.is_running(name):
             print(f"starting {Resource.INSTANCES}/{name}")
             self.session.put(self.url + Resource.INSTANCES.path() + "/" + name + "/state", json={"action": "start"})
 
     def stop(self, name: str) -> None:
+        """
+        Stops an instance if it's running (based on its name).
+
+        :param name: the instance's name
+        """
+
         if self.is_running(name):
             print(f"stopping {Resource.INSTANCES}/{name}")
             self.session.put(self.url + Resource.INSTANCES.path() + "/" + name + "/state", json={"action": "stop"})
 
     def create_stack(self, stack: dict) -> None:
-        for resource in list(Resource):
+        """
+        Creates the resources in the given stack if they don't exist (based on their name).
+        The required configurations depend on the resource's type (see zebr0_lxd.Client).
+
+        :param stack: the stack as a dictionary
+        """
+
+        for resource in list(Resource):  # order: storage pools, networks, profiles, instances
             for config in stack.get(resource) or []:
                 self.create(resource, config)
 
     def delete_stack(self, stack: dict) -> None:
-        for resource in reversed(Resource):
+        """
+        Deletes the resources in the given stack if they exist (based on their name).
+
+        :param stack: the stack as a dictionary
+        """
+
+        for resource in reversed(Resource):  # order: instances, profiles, networks, storage pools
             for config in stack.get(resource) or []:
                 self.delete(resource, config.get("name"))
 
     def start_stack(self, stack: dict) -> None:
+        """
+        Starts the instances in the given stack if they're not running (based on their name).
+
+        :param stack: the stack as a dictionary
+        """
+
         for config in stack.get(Resource.INSTANCES) or []:
             self.start(config.get("name"))
 
     def stop_stack(self, stack: dict) -> None:
+        """
+        Stops the instances in the given stack if they're running (based on their name).
+
+        :param stack: the stack as a dictionary
+        """
+
         for config in stack.get(Resource.INSTANCES) or []:
             self.stop(config.get("name"))
 
